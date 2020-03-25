0b32b0eec9a0cab3a917737679d7c2f4fbc3f094
smnwltr <smn@wltr.co>
2020-03-24 19:25:23 -0600

    Make navbar see through until scroll

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,7 +2,21 @@
 // Insert year for copyright
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
+// Add white background to navbar upon scroll
 
+$(document).ready(function () {
+    //caches a jQuery object containing the header element
+    var header = $(".navbar");
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 250) {
+            header.addClass("navbar-scroll");
+        } else {
+            header.removeClass("navbar-scroll")
+        }
+    });
+});
 
 // Hamburger menu toggling
 window.onload = function () {
