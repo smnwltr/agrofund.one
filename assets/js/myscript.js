2e891d94a52b42ee6534a8cc95071fee57318bc7
smnwltr <smn@wltr.co>
2020-04-14 13:50:00 -0600

    Update to new structure and content

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -7,6 +7,7 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 $(document).ready(function () {
     //caches a jQuery object containing the header element
     var header = $(".navbar");
+    var links = $(".nav-item");
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
@@ -18,29 +19,16 @@ $(document).ready(function () {
     });
 });
 
-// video control
-
-// video custom controls
-var video = document.querySelector('.bp-video');
-var btnMute = document.getElementById('mute-unmute-btn');
-
-// mute unmute function
-
-function toggleMuteUnmute() {
-    if (video.muted) {
-        video.muted = false;
-        btnMute.className = 'mute'
-    }
-    else {
-        video.muted = true;
-        btnMute.className = 'unmute'
-    }
-}
-
-btnMute.onclick = function () {
-    toggleMuteUnmute();
-}
-
+// smooth scrolling
+$(document).ready(function () {
+    $(".smooth-scroll").click(function (e) {
+        e.preventDefault();
+        var position = $($(this).attr("href")).offset().top;
+        $("html, body").animate({
+            scrollTop: position
+        }, 1000);
+    });
+});
 
 // Hamburger menu toggling
 window.onload = function () {
@@ -60,50 +48,3 @@ window.onload = function () {
         });
     });
 }
-
-
-// smooth scrolling
-$(document).ready(function () {
-    $(".smooth-scroll").click(function (e) {
-        e.preventDefault();
-
-        var position = $($(this).attr("href")).offset().top;
-
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('.scrolltop:hidden').stop(true, true).fadeIn();
-        } else {
-            $('.scrolltop').stop(true, true).fadeOut();
-        }
-    });
-    $(function () { $(".scroll").click(function () { $("html,body").animate({ scrollTop: 0 }, 1000); return false }) })
-
-});
-
-
-// Adjusting scroll to for anchor tags so they are not hidden by navbar, 
-// thanks to https://stackoverflow.com/a/17535094/11159842
-
-// The function actually applying the offset
-function offsetAnchor() {
-    if (location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - 100);
-    }
-}
-
-// Captures click events of all <a> elements with href starting with #
-$(document).on('click', 'a[href^="#"]', function (event) {
-    // Click events are captured before hashchanges. Timeout
-    // causes offsetAnchor to be called after the page jump.
-    window.setTimeout(function () {
-        offsetAnchor();
-    }, 0);
-});
-
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
